af1d2af51b18bdadf022f052d4509b287d593cd8
lekhanageetha1411-coder <lekhanageetha1411@gmail.com>
2026-04-02 08:28:30 -0700

    Add basic arithmetic and square root calculations

---
+++ ex.py
@@ -0,0 +1,11 @@
+1.num1 = int(input("Enter num1:"))
+num2 = int(input("Enter num2:"))
+
+sum = num1 + num2
+
+print("the sum of {0} and {1} is {2}".format(num1,num2,sum))
+
+2.num1 = float(input("Enter num1"))
+sq_rt = num1 ** 0.5
+
+print("Square root of %0.3f  is %0.2f"%(num1,sq_rt))
