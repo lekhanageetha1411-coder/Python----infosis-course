268d335ebec6f0b804180d9a372141d7b3148f04
lekhanageetha1411-coder <lekhanageetha1411@gmail.com>
2026-04-02 09:31:13 -0700

    Implement comparison of two numbers for largest value

---
+++ day2.py
@@ -0,0 +1,41 @@
+#cel = float(input("Enter the celcius value:"))
+
+#farh = (cel * 1.8) +32
+
+#print("%0.2f celcius is equal to %0.2f farhenit"%(cel,farh))
+#farh = float(input("Enter the value fo farhenit:"))
+
+#cel = (farh - 32) / 1.8
+
+#print("%0.2f farhenit is equal to %0.2f celcius"%(farh,cel))
+
+#num1 = int(input("Enter the number value:"))
+
+#if num1 > 0:
+    #print("Entered number is +ve")
+#else:
+    #print("entered number is -ve")    
+#num1 = int(input("Enter any value bwt 1 to 100:"))
+
+#if num1 %2 ==0:
+    #print("{0} is even number".format(num1))
+#else:
+    #print("{0} is odd number".format(num1))    
+
+num1 = int(input("Enter the num1 :"))
+num2 = int(input("Enter the num2 :"))
+#num3 = int(input("Enter the num3 :"))
+
+#if (num1 > num2 and num1 > num3):
+    #large = num1
+#elif(num2 > num3 and num2 > num1)    :
+    #large = num2
+#else:
+    #large = num3
+
+#print("The large value is :",large)        
+
+if num1 > num2:
+    print("largest:",num1)
+else:
+    print("largest",num2)    
